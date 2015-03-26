e548b904bf6fd0baca9d06ef1e6d788a88f320ba
Sean Hughes <smhughes@uw.edu>
2015-03-26 10:17:01 -0700

    Only return rows that have non-NA values

--- R/read.plate.R
+++ R/read.plate.R
@@ -3,4 +3,8 @@ read.plate <- function(plateSize, wellIdsColumn, filename, columnName) {
    data <- data.frame(w = getWellIds(plateSize))
    colnames(data) <- wellIdsColumn
    result <- addPlate(data, plateSize, wellIdsColumn, filename, columnName)
+   
+   # only return rows which have value for more than the well ID
+   nonNA <- apply(result, 1, FUN = function(x) sum(!is.na(x)))
+   result[nonNA > 1, ]
 }
