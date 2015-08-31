eb988b741136936e247a4882b4bbd913ab4160a8
Sean Hughes <smhughes@uw.edu>
2015-08-31 12:50:30 -0700

    Update tests for convertOnePlate to work with single files

--- tests/testthat/test-convertOnePlate.R
+++ tests/testthat/test-convertOnePlate.R
@@ -4,39 +4,45 @@ for (i in c(12, 24, 48, 96, 384)) {
    ################################################################################
    context("testing convertOnePlate-validatePlate()")
    ################################################################################
+   getFileForValidatePlate <- function(file) {
+      result <- readLines(paste0(path, file))
+      
+      # remove top row
+      result[2:(length(result))]
+   }
    
    test_that("validate plate fails for incorrect plate dimensions", {
-      bottomRow <- readPlate(paste0(path, "missingBottomRow.csv"))
+      bottomRow <- readPlate(getFileForValidatePlate("missingBottomRow.csv"))
       expect_that(validatePlate(bottomRow), throws_error())   
       
-      rightColumn <- readPlate(paste0(path, "missingRightColumn.csv"))
+      rightColumn <- readPlate(getFileForValidatePlate("missingRightColumn.csv"))
       expect_that(validatePlate(rightColumn), throws_error())
       
-      missingMiddle <- readPlate(paste0(path, "missingMiddleRow.csv"))
+      missingMiddle <- readPlate(getFileForValidatePlate("missingMiddleRow.csv"))
       expect_that(validatePlate(missingMiddle), throws_error())   
      
-      extraRow <- readPlate(paste0(path, "oneExtraRow.csv"))
+      extraRow <- readPlate(getFileForValidatePlate("oneExtraRow.csv"))
       expect_that(validatePlate(extraRow), throws_error())   
       
-      extraCol <- readPlate(paste0(path, "oneExtraColumn.csv"))
+      extraCol <- readPlate(getFileForValidatePlate("oneExtraColumn.csv"))
       expect_that(validatePlate(extraCol), throws_error())
    })
    
    test_that("validatePlate() fails for incorrect row labels", {
-      wrongRowLabels <- readPlate(paste0(path, "incorrectRowLabels.csv"))
+      wrongRowLabels <- readPlate(getFileForValidatePlate("incorrectRowLabels.csv"))
       expect_that(validatePlate(wrongRowLabels), throws_error()) 
       
-      wrongRowLabels <- readPlate(paste0(path, "missingRowLabels.csv"))
+      wrongRowLabels <- readPlate(getFileForValidatePlate("missingRowLabels.csv"))
       expect_that(validatePlate(wrongRowLabels), throws_error()) 
    })
    
    test_that("validatePlate() passes with valid input", {
       # no error
-      plate <- readPlate(paste0(path, "validPlate.csv"))
+      plate <- readPlate(getFileForValidatePlate("validPlate.csv"))
       validatePlate(plate, i)
       
       # missing column data, but includes all titles
-      plate <- readPlate(paste0(path, "missingColumnsWithCorrectTitles.csv"))
+      plate <- readPlate(getFileForValidatePlate("missingColumnsWithCorrectTitles.csv"))
       validatePlate(plate, i)
    })
    
@@ -45,12 +51,12 @@ for (i in c(12, 24, 48, 96, 384)) {
    ################################################################################
    
    test_that("wrongRowLabelsErrorMessage() fails for invalid plate dimensions", {
-      missingRow <- readPlate(paste0(path, "missingBottomRow.csv"))
+      missingRow <- readPlate(getFileForValidatePlate("missingBottomRow.csv"))
       expect_that(wrongRowLabelsErrorMessage(validPlate, i), throws_error())
    })
    
    test_that("wrongRowLabelsErrorMessage()", {
-      incorrectRowLabels <- readPlate(paste0(path, "incorrectRowLabels.csv"))
+      incorrectRowLabels <- readPlate(getFileForValidatePlate("incorrectRowLabels.csv"))
       message <- wrongRowLabelsErrorMessage(incorrectRowLabels, i)
       
       rows <- numberOfRows(i)
@@ -66,19 +72,23 @@ for (i in c(12, 24, 48, 96, 384)) {
    ################################################################################
    context("testing convertOnePlate-convertOnePlate")
    ################################################################################
+   getFileForConvertPlate <- function(file) {
+      readLines(paste0(path, file))
+   }
+   
    test_that("convertOnePlate() gives correct output", {
       # every well present, all have own ID as contents
-      plate <- convertOnePlate(paste0(path, "allWellIds.csv"), i, "contents")
-      expect_that(plate$contents, is_identical_to(plate$wellIds))
+      plate <- convertOnePlate(getFileForConvertPlate("allWellIds.csv"), i)   
+      expect_that(plate$values, is_identical_to(plate$wellIds))
    })
    
    test_that("convertOnePlate() gives correct output with some empty wells", {
       # every well present, all have own ID as contents
-      plate <- convertOnePlate(paste0(path, "wellIdsAndEmptyWells.csv"), i, "contents")
+      plate <- convertOnePlate(getFileForConvertPlate("wellIdsAndEmptyWells.csv"), i)
       expectedNAs <- c("12" = 1, "24" = 1, "48" = 5, "96" = 27, "384" = 30)
-      expect_that(sum(is.na(plate$contents)), 
+      expect_that(sum(is.na(plate$values)), 
          is_equivalent_to(expectedNAs[as.character(i)]))
-      plate <- plate[!is.na(plate$contents), ]
-      expect_that(plate$contents, is_identical_to(plate$wellIds))
+      plate <- plate[!is.na(plate$values), ]
+      expect_that(plate$values, is_identical_to(plate$wellIds))
    })
 }
